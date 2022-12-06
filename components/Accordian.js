40834712fb0d1f1b39b2b1ab33c65636d3d29c1d
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-12-06 20:21:37 +0545

    added new components

---
+++ components/Accordian.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
+
+const Accordian = ({ header, body }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="w-[25em]">
+      <div
+        style={{ background: "rgb(254, 214, 16)" }}
+        className="bg-rose-100 border-gray-200 p-5 rounded-md mb-2"
+      >
+        <div className="flex justify-between ">
+          <h2 className="font-semibold">{header}</h2>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            {isOpen ? <AiOutlineUp /> : <AiOutlineDown />}
+          </button>
+        </div>
+      </div>
+      <div
+        className={`mb-2 text-justify p-2 rounded-md shadow-lg  ${
+          isOpen ? "visible" : "hidden"
+        }`}
+      >
+        <p className="p-2">{body}</p>
+      </div>
+    </div>
+  );
+};
+
+const AccordianLayout = ({ items }) => {
+  return (
+    <div className="bg-white p-8 rounded lg:hidden border-neutral-300">
+      {items.map((el, i) => (
+        <Accordian key={i} header={el.header} body={el.body} />
+      ))}
+    </div>
+  );
+};
+
+export default AccordianLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-import { server } from "../config";
-import ArticleList from "../components/ArticleList";
-
-export default function Home({ articles }) {
+import AccordianLayout from "../components/Accordian";
+import Tabs from "../components/Tabs";
+import FAQ from "../utils/FAQ.json";
+export default function Home() {
+  const items = FAQ.items.map((el) => {
+    return {
+      header: el.name,
+      body: el.acceptedAnswer.text,
+    };
+  });
   return (
-    <div>
-      <ArticleList articles={articles} />
+    <div className=" h-screen mt-32 flex flex-col w-full items-center">
+      <AccordianLayout items={items} />
+      <Tabs items={items} />
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`);
-  const articles = await res.json();
-
-  return {
-    props: {
-      articles,
-    },
-  };
-};
